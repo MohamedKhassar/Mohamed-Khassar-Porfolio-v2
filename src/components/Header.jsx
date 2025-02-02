a4a256fe2bf089bb1e30cb3c1f5ea1b1f492d7ba
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-02-02 16:08:20 +0100

    finish the header component with fixed navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import { FiMenu, FiX } from "react-icons/fi"
+import { Link } from "react-router-dom"
+import Navbar from "./Navbar"
+import { useState } from "react"
+
+const Header = () => {
+    const [navOpen, setNavOpen] = useState(false)
+    return (
+        <header className="h-20 fixed top-0 left-0 w-full flex items-center bg-gradient-to-b from-stone-900 to-stone-900/0">
+            <div className="flex items-center mx-auto justify-between w-full px-4 md:px-6 md:grid md:grid-cols-[1fr_3fr_1fr]">
+                <Link to={"/"} >
+                    <h1>
+                        <img src="/images/logo.svg" width={30} height={30} alt="" />
+                    </h1>
+                </Link>
+                <div className="md:justify-self-center relative">
+                    <button onClick={() => setNavOpen(pre => !pre)} className="menu-btn">
+                        {
+                            navOpen ?
+                                <FiX className="size-5" />
+                                :
+                                <FiMenu className="size-5" />
+                        }
+                    </button>
+                    <Navbar navOpen={navOpen} />
+                </div>
+                <Link to={"/#contact"} className="btn btn-secondary md:justify-self-center capitalize max-md:hidden">
+                    contact me
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Header
